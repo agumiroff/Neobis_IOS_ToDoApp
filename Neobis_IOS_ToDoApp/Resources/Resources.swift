31d0d8560a2a6ad52088c479d74e396f794b918a
Askhat Gumirov <agumirovz@gmail.com>
2023-04-03 21:14:45 +0600

    presenter added

---
+++ Neobis_IOS_ToDoApp/Resources/Resources.swift
@@ -0,0 +1,17 @@
+//
+//  Resources.swift
+//  Neobis_IOS_ToDoApp
+//
+//  Created by G G on 03.04.2023.
+//
+
+import Foundation
+
+enum Resources {
+    enum AppState {
+        case loading
+        case success
+        case failure
+        case edit
+    }
+}
