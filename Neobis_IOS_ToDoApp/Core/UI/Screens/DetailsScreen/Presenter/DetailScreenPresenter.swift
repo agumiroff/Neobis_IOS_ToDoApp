6672e37a1996f209df746e31bd9e1601347b2142
Askhat Gumirov <agumirovz@gmail.com>
2023-04-05 12:59:42 +0600

    detail screen UI and business logic added

---
+++ Neobis_IOS_ToDoApp/Core/UI/Screens/DetailsScreen/Presenter/DetailScreenPresenter.swift
@@ -0,0 +1,46 @@
+//
+//  DetailScreenPresenter.swift
+//  Neobis_IOS_ToDoApp
+//
+//  Created by G G on 05.04.2023.
+//
+
+import Foundation
+
+protocol DetailScreenPresenterProtocol: AnyObject {
+    func viewDidLoad()
+    func cancelToDo()
+    func saveToDo(title: String, description: String)
+    var toDoItem: ToDoModel? { get set }
+}
+
+class DetailScreenPresenter: DetailScreenPresenterProtocol {
+    weak var view: DetailScreenViewControllerProtocol?
+    var toDoService: ToDoStorageServiceProtocol!
+    var router: RouterProtocol?
+    var toDoItem: ToDoModel?
+    var toDoIndex: Int?
+    
+    func viewDidLoad() {
+        
+        
+    }
+    
+    func cancelToDo() {
+        router?.mainScreenRoute()
+    }
+    
+    func saveToDo(title: String, description: String) {
+        if toDoIndex == nil {
+            toDoService.addTodo(toDo: ToDoModel(title: title,
+                                                Description: description,
+                                                isComplete: false))
+        } else {
+            toDoService.saveTodo(toDo: ToDoModel(title: title,
+                                                 Description: description,
+                                                 isComplete: toDoItem?.isComplete ?? false),
+                                 index: toDoIndex ?? -1)
+        }
+    }
+        
+}
---
+++ Neobis_IOS_ToDoApp/Core/UI/Screens/DetailsScreen/View/DetailScreenViewController.swift
@@ -0,0 +1,126 @@
+//
+//  DetailsScreen.swift
+//  Neobis_IOS_ToDoApp
+//
+//  Created by G G on 03.04.2023.
+//
+
+import Foundation
+import UIKit
+
+protocol DetailScreenViewControllerProtocol: AnyObject {
+    
+}
+
+class DetailScreenViewController: UIViewController,
+                                  DetailScreenViewControllerProtocol,
+                                    UITextViewDelegate {
+    
+    var presenter: DetailScreenPresenterProtocol?
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Название"
+        return label
+    }()
+    
+    private let titleField: UITextField = {
+        let field = UITextField()
+        field.placeholder = "Название"
+        field.backgroundColor = .white
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 8, height: 40))
+        field.leftViewMode = .always
+        field.rightView = UIView(frame: CGRect(x: 0, y: 0, width: 8, height: 40))
+        field.rightViewMode = .always
+        return field
+    }()
+    
+    private let textView: UITextView = {
+        let textView = UITextView()
+        return textView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray5
+        navigationControllerSetup()
+        setupViews()
+    }
+    
+}
+
+//MARK: Views setup
+extension DetailScreenViewController {
+    
+    func navigationControllerSetup() {
+        navigationController?.isNavigationBarHidden = false
+        
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Отмена",
+                                                           style: .plain,
+                                                           target: self,
+                                                           action: #selector(cancelToDo))
+        navigationItem.leftBarButtonItem?.tintColor = .red
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Сохранить",
+                                                            image: .none,
+                                                            target: self,
+                                                            action: #selector(saveToDo))
+    }
+    
+    private func setupViews() {
+        titleLabelSetup()
+        titleFieldSetup()
+        textViewSetup()
+    }
+    
+    private func titleLabelSetup() {
+        
+        view.addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.left.equalTo(view.safeAreaLayoutGuide).inset(30)
+        }
+    }
+    
+    private func titleFieldSetup() {
+        view.addSubview(titleField)
+        
+        titleField.text = presenter?.toDoItem?.title
+        
+        titleField.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(5)
+            make.left.right.equalTo(view.safeAreaLayoutGuide).inset(30)
+            make.height.equalTo(40)
+        }
+    }
+    
+    private func textViewSetup() {
+        textView.delegate = self
+        view.addSubview(textView)
+        
+        textView.text = presenter?.toDoItem?.description
+        
+        textView.snp.makeConstraints { make in
+            make.left.right.bottom.equalToSuperview().inset(30)
+            make.top.equalTo(titleField.snp.bottom).offset(20)
+        }
+    }
+    
+}
+
+extension DetailScreenViewController {
+    @objc func saveToDo() {
+        if textView.text.isEmpty {
+            return
+        }
+        presenter?.saveToDo(title: titleField.text ?? "",
+                            description: textView.text)
+        presenter?.cancelToDo()
+    }
+    
+    @objc func cancelToDo() {
+        presenter?.cancelToDo()
+    }
+}
